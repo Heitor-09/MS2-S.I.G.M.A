92cb6cec3bf620f1399bf9ba94e1228a37f1f9b4
Heitor-09 <heitor.b.santos6@aluno.senai.br>
2026-09-04 15:45:33 -0300

    Criação do Sistema de Contador de Migués Realizados

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,36 @@
 const btnMigue = document.getElementById("btnMigue");
 const migue = document.getElementById("migue");
 const mensagem = document.getElementById("mensagem");
+const contador = document.getElementById("contador");
 
 const desculpas = [
-  "Não consegui ir porque surgiu um compromisso familiar.",
-  "Meu despertador não tocou.",
-  "Tive um problema com a internet.",
-  "O ônibus atrasou muito.",
-  "Esqueci completamente que tinha esse compromisso.",
-  "Passei mal durante a noite.",
-  "Tive um imprevisto de última hora.",
-  "Meu computador parou de funcionar.",
-  "Achei que o prazo era amanhã.",
-  "Tive que resolver uma situação urgente.",
+    "Não consegui ir porque surgiu um compromisso familiar.",
+    "Meu despertador não tocou.",
+    "Tive um problema com a internet.",
+    "O ônibus atrasou muito.",
+    "Esqueci completamente que tinha esse compromisso.",
+    "Passei mal durante a noite.",
+    "Tive um imprevisto de última hora.",
+    "Meu computador parou de funcionar.",
+    "Achei que o prazo era amanhã.",
+    "Tive que resolver uma situação urgente."
 ];
 
+let totalMigue = 0;
+
 btnMigue.addEventListener("click", function () {
-  const indice = Math.floor(Math.random() * desculpas.length);
+    const indice = Math.floor(Math.random() * desculpas.length);
 
-  mensagem.textContent = desculpas[indice];
+    mensagem.textContent = desculpas[indice];
 
-  migue.classList.remove("d-none");
-  migue.classList.remove("animacao-alert");
+    totalMigue++;
 
-  void migue.offsetWidth;
+    contador.textContent = totalMigue;
 
-  migue.classList.add("animacao-alert");
+    migue.classList.remove("d-none");
+    migue.classList.remove("animacao-alert");
+
+    void migue.offsetWidth;
+
+    migue.classList.add("animacao-alert");
 });
